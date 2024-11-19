7a3e527112d7ea22ec8e99184b0b2efa4622c329
Ryan Taylor Long <ryan@rtlong.com>
2024-11-19 08:30:59 -0500

    Pull in my tmux and htop configs

--- modules/home/suites/common/default.nix
+++ modules/home/suites/common/default.nix
@@ -47,6 +47,54 @@ in
         enable = true;
         keyMode = "vi";
         shortcut = "<space>";
+        baseIndex = 1;
+        clock24 = true;
+        historyLimit = 10000;
+        mouse = true;
+        newSession = true;
+        prefix = "C-Space";
+        shell = "${pkgs.zsh}/bin/zsh";
+
+        extraConfig = ''
+          unbind-key  C-Down
+          unbind-key  C-Left
+          unbind-key  C-Right
+          unbind-key  C-Up
+          bind-key     o              last-window
+          bind-key     C-c            new-window -c "#{pane_current_path}"
+          bind-key     c              new-window
+          bind-key -r  h              select-pane -L
+          bind-key -r  j              select-pane -D
+          bind-key -r  k              select-pane -U
+          bind-key -r  l              select-pane -R
+          bind-key -r  p              previous-window
+          bind-key -r  n              next-window
+          bind-key     s              split-window
+          bind-key     C-s            split-window -c "#{pane_current_path}"
+          bind-key     v              split-window -h
+          bind-key     C-v            split-window -h -c "#{pane_current_path}"
+
+          set-option -g   renumber-windows              on
+          set-option -g   visual-activity               on
+          set-option -g   visual-bell                   off
+          set-option -g   visual-silence                on
+          set-option -s   focus-events                  on
+          set-option -wg  automatic-rename              on
+          set-option -g   detach-on-destroy             on
+          set-option -wg  pane-active-border-style      'bg=black,fg=red'
+          set-option -wg  pane-border-style             'bg=black,fg=#999999'
+          set-option -g   message-command-style         'fg=black,bg=yellow'
+          set-option -g   message-style                 'fg=black,bg=yellow'
+          set-option -wg  window-status-current-style   'bg=#333333,fg=yellow'
+          set-option -wg  window-status-last-style      'fg=yellow'
+          set-option -g   status-interval               5
+          set-option -g   status-left                   "#{?client_prefix,#[bg=magenta]#[fg=black],}[#S]#[default] "
+          set-option -g   status-right                  "#(loadavg | awk '{print $1}')x #[fg=green]#{=21:pane_title} #[fg=cyan]#H #[fg=red]%H:%M %d-%b"
+          set-option -g   status-right-length           60
+          set-option -g   status-style                  'fg=#ffffff,bg=#111111'
+          set-option -g   set-titles on
+          set-option -g   set-titles-string '#{window_name}:#{pane_title}'
+        '';
       };
 
       # # Htop
@@ -55,7 +103,38 @@ in
         enable = true;
         settings = {
           show_program_path = true;
-        };
+          color_scheme = 6;
+          cpu_count_from_one = 0;
+          delay = 15;
+          fields = with config.lib.htop.fields; [
+            PID
+            USER
+            PRIORITY
+            NICE
+            M_SIZE
+            M_RESIDENT
+            M_SHARE
+            STATE
+            PERCENT_CPU
+            PERCENT_MEM
+            TIME
+            COMM
+          ];
+          highlight_base_name = 1;
+          highlight_megabytes = 1;
+          highlight_threads = 1;
+        } // (with config.lib.htop; leftMeters [
+          (bar "AllCPUs2")
+          (bar "Memory")
+          (bar "Swap")
+          (text "Zram")
+        ]) // (with config.lib.htop; rightMeters [
+          (text "Tasks")
+          (text "LoadAverage")
+          (text "Uptime")
+          (text "Systemd")
+        ]);
+
       };
 
       home.shellAliases =
@@ -97,6 +176,8 @@ in
           autoload -U select-word-style
           select-word-style bash
           local WORDCHARS='*?_[]~=&;!#$%^(){}<>'
+
+          setopt extendedglob
         '';
 
         syntaxHighlighting.enable = true;
@@ -107,6 +188,8 @@ in
         ];
         history.extended = true;
         historySubstringSearch.enable = true;
+
+
       };
 
       programs.starship = {
@@ -149,7 +232,6 @@ in
         gitFull
         nawk
         findutils
-        htop
         dig
         openssh
         # nmap
