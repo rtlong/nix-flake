f6c7c1548443852fcd8d18809057128768ff17a5
Ryan Taylor Long <ryan@rtlong.com>
2024-11-20 13:00:44 -0500

    fix aerospace module

--- modules/home/aerospace/default.nix
+++ modules/home/aerospace/default.nix
@@ -1,61 +1,76 @@
-# { pkgs, lib, ... }:
+{ config, namespace, pkgs, lib, ... }:
 
-# let
-#   settingsFormat = pkgs.formats.toml { };
+let
+  settingsFormat = pkgs.formats.toml { };
 
-#   settings = {
-#     start-at-login = true;
+  defaultSettings = {
+    start-at-login = true;
 
-#     enable-normalization-flatten-containers = false;
-#     enable-normalization-opposite-orientation-for-nested-containers = false;
+    enable-normalization-flatten-containers = false;
+    enable-normalization-opposite-orientation-for-nested-containers = false;
 
-#     default-root-container-layout = "tiles";
-#     default-root-container-orientation = "auto";
+    default-root-container-layout = "tiles";
+    default-root-container-orientation = "auto";
 
-#     gaps = {
-#       inner = {
-#         horizontal = 10;
-#         vertical = 10;
-#       };
+    gaps = {
+      inner = {
+        horizontal = 10;
+        vertical = 10;
+      };
 
-#       outer = {
-#         left = 10;
-#         bottom = 10;
-#         top = 10;
-#         right = 10;
-#       };
-#     };
+      outer = {
+        left = 10;
+        bottom = 10;
+        top = 10;
+        right = 10;
+      };
+    };
 
-#     key-mapping = {
-#       preset = "qwerty";
-#     };
+    key-mapping = {
+      preset = "qwerty";
+    };
 
-#     mode.main.binding =
-#       let
-#         meh = "ctrl-alt-shift";
-#       in
-#       {
-#         "${meh}-h" = "move left";
-#         "${meh}-j" = "move down";
-#         "${meh}-k" = "move up";
-#         "${meh}-l" = "move right";
+    mode.main.binding =
+      let
+        meh = "ctrl-alt-shift";
+      in
+      {
+        "${meh}-h" = "move left";
+        "${meh}-j" = "move down";
+        "${meh}-k" = "move up";
+        "${meh}-l" = "move right";
 
-#         "${meh}-w" = "move-node-to-monitor --wrap-around left";
-#         "${meh}-e" = "move-node-to-monitor --wrap-around right";
+        "${meh}-w" = "move-node-to-monitor --wrap-around left";
+        "${meh}-e" = "move-node-to-monitor --wrap-around right";
 
-#         "${meh}-f" = "fullscreen";
+        "${meh}-f" = "fullscreen";
 
-#         "${meh}-t" = "layout floating tiling";
-#       };
-#   };
-# in
-# {
-#   home.packages = [ pkgs.aerospace ];
+        "${meh}-t" = "layout floating tiling";
+      };
+  };
 
-#   xdg.configFile."aerospace/aerospace.toml".source = settingsFormat.generate "aerospace.toml" settings;
+  inherit (lib) mkIf mkOption types;
+  inherit (lib.${namespace}) mkBoolOpt;
+  cfg = config.${namespace}.aerospace;
+in
+{
+  options.${namespace}.aerospace = {
+    enable = mkBoolOpt false "Enable aerospace WM";
+    config = mkOption {
+      type = types.attrsOf types.any;
+      default = defaultSettings;
+      description = "Settings for aerospace to be written as the aerospace.toml file";
+    };
+  };
 
-#   home.activation.aeorspaceConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] /* bash */ ''
-#     $VERBOSE_ECHO "Reloading configuration"
-#     $DRY_RUN_CMD ${pkgs.aerospace}/bin/aerospace reload-config
-#   '';
-# }
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.aerospace ];
+
+    xdg.configFile."aerospace/aerospace.toml".source = settingsFormat.generate "aerospace.toml" cfg.config;
+
+    home.activation.aeorspaceConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] /* bash */ ''
+      $VERBOSE_ECHO "Reloading configuration"
+      $DRY_RUN_CMD ${pkgs.aerospace}/bin/aerospace reload-config
+    '';
+  };
+}
