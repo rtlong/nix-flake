9504d90b02eef2a970a9d37895676d5461c22673
Ryan Taylor Long <ryan@rtlong.com>
2025-02-12 00:11:51 -0500

    Get odroid-n2+ device working

--- modules/home/suites/common/default.nix
+++ modules/home/suites/common/default.nix
@@ -187,6 +187,10 @@ in
       programs.ssh = {
         enable = true;
 
+        includes = [
+          "~/.ssh/config.d/*"
+        ];
+
         matchBlocks = {
           "*".extraOptions = {
             IdentityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
