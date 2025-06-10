7bd8c0d75e4223b66b853287899fc41c19bed6d2
Ryan Taylor Long <ryan@rtlong.com>
2025-06-10 14:56:48 -0400

    Fix minor issues with open-webui

--- packages/open-webui/default.nix
+++ packages/open-webui/default.nix
@@ -7,9 +7,12 @@
 
 (pkgs.writeShellApplication {
   name = "open-webui";
+  runtimeInputs = with pkgs; [
+    ffmpeg
+  ];
   text = ''
     set -x
-    export DATA_DIR="$HOME/.open-webui"
+    export DATA_DIR="''${OPEN_WEBUI_DATA_DIR:-$HOME/.open-webui}"
     export UV_NO_MANAGED_PYTHON=true
     export UV_PYTHON="${pkgs.python311}"
     cd "$DATA_DIR"
